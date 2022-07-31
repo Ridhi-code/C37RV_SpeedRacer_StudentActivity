f9e793ecc240097580d61ddb343e12c9c819abd8
gudiya47 <83075306+Ridhi-code@users.noreply.github.com>
2022-07-31 19:00:39 +0530

    c37

--- js/Game.js
+++ js/Game.js
@@ -7,6 +7,11 @@ class Game {
       gameState = data.val();
     });
   }
+  update(state) {
+    database.ref("/").update({
+      gameState: state
+    });
+  }
 
   start() {
     player = new Player();
